de61bb3adbbce4965488a10045561ce65c8177be
Hossein <djalicrt@gmail.com>
2022-04-19 13:41:40 +0430

    impl. Mobile Controller For PingPong Rocket! :)

--- Assets/Scripts/Players.cs
+++ Assets/Scripts/Players.cs
@@ -12,6 +12,8 @@ public class Players : MonoBehaviour
     private float move;
     private Vector2 startPos;
 
+    private float moveSpeedTouchControl = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +22,42 @@ public class Players : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    
     {
         if (player1) {
-            move = Input.GetAxisRaw("Vertical");
+            
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                if (mousePos.x > 1) {
+                    if(mousePos.y > 0.5) {
+                        transform.Translate(0,moveSpeedTouchControl,0);
+                    }
+                    else if (mousePos.y < -0.5) {
+                         transform.Translate(0,-moveSpeedTouchControl,0);
+                    }
+                }
+            }
+
+
+            // move = Input.GetAxisRaw("Vertical");
         }
         else {
-            move = Input.GetAxisRaw("Vertical2");
+
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                if (mousePos.x < -1) {
+                    if(mousePos.y > 0.5) {
+                        transform.Translate(0,moveSpeedTouchControl,0);
+                    }
+                    else if (mousePos.y < -0.5) {
+                         transform.Translate(0,-moveSpeedTouchControl,0);
+                    }
+                }
+            }
+
+            // move = Input.GetAxisRaw("Vertical2");
         }
 
         rb.velocity = new Vector2(rb.velocity.x , move*speed);
